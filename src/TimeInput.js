cb8fd556df61d37ca1b8a5f10a8f297abceddd53
guidugaich <guidugaich.dev@gmail.com>
2021-05-23 21:08:15 -0300

    updating component TimeInput

--- src/TimeInput.js
+++ src/TimeInput.js
@@ -2,16 +2,32 @@ import React from 'react'
 
 class TimeInput extends React.Component {
   render() {
+    const { updateTimer, inputHours, inputMinutes, inputSeconds } = this.props;
     return (
       <>
         <label>H
-          <input type='number' />
+          <input
+            type='number'
+            name="inputHours"
+            onChange={ updateTimer }
+            value={ inputHours }
+          />
         </label>
         <label>M
-          <input type='number' />
+          <input
+            type='number'
+            name="inputMinutes"
+            onChange={ updateTimer }
+            value={ inputMinutes }
+          />
         </label>
         <label>S
-          <input type='number' />
+          <input
+            type='number'
+            name="inputSeconds"
+            onChange={ updateTimer }
+            value={ inputSeconds }
+          />
         </label>
       </>
     )
