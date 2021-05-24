7f645aebf2e81e340ee62536dcfe8f7afef85014
guidugaich <guidugaich.dev@gmail.com>
2021-05-23 21:07:41 -0300

    adding countdown functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import React from 'react';
 import TimeInput from './TimeInput';
+import TimeDisplay from './TimeDisplay';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      secondsRemaining: 6,
+      secondsRemaining: 0,
+      inputHours: '',
+      inputMinutes: '',
+      inputSeconds: '',
     }
   }
 
   startCountdown = () => {
-    setInterval(() => this.setState((prevState) => ({
-      secondsRemaining: prevState.secondsRemaining - 1
-    })), 1000);
-    if (this.state.secondsRemaining === 0) {
-      clearInterval();
+    const updateState = () => {
+      if (this.state.secondsRemaining > 0) {
+        this.setState((prevState) => ({
+          secondsRemaining: prevState.secondsRemaining - 1
+        }));
+      } else {
+        clearInterval(updateState);
+      }
     }
+
+    setInterval(updateState, 1000);
+
+  }
+
+  getTimeFromInput = ({ target }) => {
+    const { name } = target;
+    let { value } = target;
+
+    if (value === '') value = 0;
+
+    this.setState({[name]: parseFloat(value)}, () => {
+      this.setState({
+        secondsRemaining: (
+          this.state.inputHours * 3600 +
+          this.state.inputMinutes * 60 +
+          this.state.inputSeconds)
+      })
+    }); 
   }
 
   render() {
+    const { secondsRemaining, inputHours, inputMinutes, inputSeconds } = this.state;
+
     return (
       <>
         <h1>Countdown</h1>
         <h3>Quanto tempo você quer marcar?</h3>
-        <TimeInput />
+        <TimeInput
+          updateTimer={ this.getTimeFromInput }
+          inputHours={ inputHours }
+          inputMinutes={ inputMinutes }
+          inputSeconds={ inputSeconds }
+        />
         <button onClick={ this.startCountdown }>Start!</button>
-        <p>{ this.state.secondsRemaining }</p>
+        <TimeDisplay secondsRemaining = { secondsRemaining } />
       </>
     );
   }
