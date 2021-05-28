b69b468945006553cffc5419a050c1b1170f372c
guidugaich <guidugaich.dev@gmail.com>
2021-05-28 15:27:54 -0300

    conditional styling for TimeDisplay

--- src/TimeDisplay.js
+++ src/TimeDisplay.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import './TimeDisplay.css';
 
 class TimeDisplay extends React.Component {
   
   
   render() {
-    const { secondsRemaining } = this.props;
+    const { secondsRemaining, running } = this.props;
     const num = new Date(secondsRemaining*1000).toISOString().substr(11, 8);
+    let timeDisplayStyle;
+    let timeDisplayContainerStyle;
+    if (running) {
+      timeDisplayStyle = 'time-display-running'
+      timeDisplayContainerStyle = 'time-display-container-running'
+    } else {
+      timeDisplayStyle = 'time-display-stopped';
+      timeDisplayContainerStyle = 'time-display-container-stopped'
+    }
 
     return (
-      <>
-        <span>{ num }</span>
-      </>
+      <div className={`time-display ${timeDisplayContainerStyle}`}>
+        <div className={ timeDisplayStyle }>{ num }</div>
+      </div>
     )
   }
 }
